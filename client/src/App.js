28bce8d9469ca46e177a989bec6757cddf9299aa
Robert Lesniak <robert.lesniak@thecogworks.com>
2019-01-18 14:32:57 +0100

    added reactstrap and display car as a card comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,22 @@
 import React, { Component } from 'react';
 import NavBar from './Components/NavBar';
+import Car from './Components/Car';
+// import { Card, CardImg, CardText, CardBody,
+//   CardTitle, CardSubtitle, Button, Row, Col } from 'reactstrap';
+import './style.css';
+
 
 class App extends Component {
-  constructor(){
-    super()
-    this.state = {
-      cars: [],
-      error: null
-    }
-  }
-
-  componentDidMount(){
-    fetch('http://localhost:5000/api/car')
-      .then(response => response.json())
-      .then(data => {
-        this.setState({
-          cars: data
-        })
-      })
-      
-      
-  }
-
-  render() {
-    const {...cars} = this.state.cars;
-    console.log(cars);
+  render(){
     return (
       <div className="App">
-      <NavBar />
-      <ul>
-          {this.state.cars.map((car, key)=> <li key={key}>{car.carName}</li>)}
-      </ul>
+        <NavBar />
+        <Car />
       </div>
-    )
+    );
   }
+
 }
 
 export default App;
+
